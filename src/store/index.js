9afa93ec97b5e3b3908e9975e4ae126b60c5c13f
Kenny Q <kquevedo@classvaluation.com>
2025-08-31 23:03:45 -0500

    Added scrolling pagination and filter by api.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
-import FilterByStatus from "@/components/FilterByStatus.vue";
 import { createStore } from "vuex";
+
+const API_URL_RICK_AND_MORTY = "https://rickandmortyapi.com/api/character";
 
 export default createStore({
   state: {
     characters: [],
     charactersFilter: [],
+    totalPages: 1,
+    currentPage: 1,
+    isLoading : false,
+    selectedStatus: '',
+    characterName: '',
   },
   getters: {},
   mutations: {
@@ -14,37 +20,101 @@ export default createStore({
     setCharactersFilter(state, payload) {
       state.charactersFilter = payload;
     },
+    setTotalPages(state, totalPages) {
+      state.totalPages = totalPages;
+    },
+    setCurrentPage(state, currentPage) {
+      state.currentPage = currentPage;
+    },
+    setIsLoading(state, value) {
+      state.isLoading = value;
+    },
+    setSelectedStatus(state, status) {
+      state.selectedStatus = status;
+    },
+    setCharacterName(state, name) {
+      state.characterName = name;
+    },
   },
   actions: {
     async getCharacters({ commit }) {
       try {
-        const response = await fetch(
-          "https://rickandmortyapi.com/api/character"
-        );
+        const url = new URL(API_URL_RICK_AND_MORTY);         
+        const response = await fetch(url);
         const data = await response.json();
+
         commit("setCharacters", data.results);
         commit("setCharactersFilter", data.results);
+        commit("setTotalPages", data.info.pages);
+        commit("setCurrentPage", 1);
       } catch (error) {
         console.error(error);
       }
     },
 
-    filterByStatus({ commit, state }, status) {
-      const results = state.characters.filter((character) => {
-        return character.status.includes(status);
-      });
-      commit("setCharactersFilter", results);
+    async filterByStatus({ commit, state }, status) {
+      try {
+        const url = new URL(API_URL_RICK_AND_MORTY);
+        url.searchParams.set("page", 1);
+        commit("setCurrentPage", 1);
+        if(status) url.searchParams.set("status", status.toLowerCase());
+        if(state.characterName) url.searchParams.set("name", state.characterName);
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        commit("setCharacters", data.results);
+        commit("setCharactersFilter", data.results);
+        commit("setTotalPages", data.info.pages);
+
+        commit("setSelectedStatus", status);
+
+      } catch (error) {
+        console.error('filterByStatus_error: ', error);
+      }
     },
 
-    filterByName({ commit, state }, name) {
-      const formatName = name.toLowerCase().trim();
-      const results = state.characters.filter((character) => {
-        const characterName = character.name.toLowerCase();
-        if(characterName.includes(formatName)) {
-          return character;
-        }
-      });
-      commit("setCharactersFilter", results);
+    async filterByName({ commit, state }, name) {
+      try {
+        const url = new URL(API_URL_RICK_AND_MORTY);
+        url.searchParams.set("page", 1);
+        commit("setCurrentPage", 1);
+        if (state.selectedStatus) url.searchParams.set("status", state.selectedStatus.toLowerCase());
+        if (name) url.searchParams.set("name", name);
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        commit("setCharacters", data.results);
+        commit("setCharactersFilter", data.results);
+        commit("setTotalPages", data.info.pages);
+        commit("setCharacterName", name);
+      } catch (error) {
+        console.error('filterByName_error: ', error);
+      }
+    },
+
+    async fetchNextPage({ commit, state }) {
+      const nextPage = state.currentPage + 1;
+      if (nextPage > state.totalPages) return; // No more pages to fetch.
+      commit("setIsLoading", true); // Activate loading.
+      try {
+        const url = new URL(API_URL_RICK_AND_MORTY);
+        url.searchParams.set("page", nextPage);
+        if(state.selectedStatus) url.searchParams.set("status", state.selectedStatus.toLowerCase());
+        if(state.characterName) url.searchParams.set("name", state.characterName);
+        const response = await fetch(url);
+        
+        const data = await response.json();
+        const newCharacters = [...state.characters, ...data.results];
+        commit("setCharacters", newCharacters);
+        commit("setCharactersFilter", newCharacters);
+        commit("setCurrentPage", nextPage);
+      } catch (error) {
+        console.error("fetchNextPage error:", error);
+      } finally {
+        commit("setIsLoading", false); // Deactivate loading.
+      }
     }
   },
   modules: {},
